d890b1f64932bef3a457920900e415a0badcc47d
Alex de Mulder <alexdemulder@gmail.com>
2018-09-24 17:57:27 +0200

    removed posting substates to snitch

--- src/hourlyInvoke.js
+++ src/hourlyInvoke.js
@@ -1,23 +1,16 @@
 let util           = require('./util/util')
 let scheduledTasks = require('./scheduledTasks')
 let evaluate       = require('./execute')
-let config      = require('./config/config.' + (process.env.NODE_ENV || 'local'));
-const fetch        = require('node-fetch');
 
 function hourlyInvoke() {
-
-  let headers     = {'Content-Type': 'application/x-www-form-urlencoded'};
-  let fetchConfig = {method: 'GET', headers};
 
   console.log("Starting...")
   util.promiseBatchPerformer(scheduledTasks, (task) => { return evaluate(task) })
     .then(() => {
       console.log("Ran successfully")
-      fetch(config.snitchUrl + '?m="Success."', fetchConfig);
     })
     .catch((err) => {
       console.log("Failing with errors...", err)
-      fetch(config.snitchUrl + '?m="Failed with errors."', fetchConfig);
     })
 
 }
