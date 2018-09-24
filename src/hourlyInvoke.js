5c20e05a794c5cc3b172b0c061b897ef592922f4
Alex de Mulder <alexdemulder@gmail.com>
2018-09-24 17:12:52 +0200

    removed idea folder

--- src/hourlyInvoke.js
+++ src/hourlyInvoke.js
@@ -3,14 +3,21 @@ let scheduledTasks = require('./scheduledTasks')
 let evaluate       = require('./execute')
 const fetch        = require('node-fetch');
 
-let config      = require('./config/config.' + (process.env.NODE_ENV || 'local'));
-let headers     = {'Content-Type': 'application/x-www-form-urlencoded'};
-let fetchConfig = {method: 'GET', headers};
+function hourlyInvoke() {
 
-util.promiseBatchPerformer(scheduledTasks, (task) => { return evaluate(task) })
-  .then(() => {
-    fetch(config.snitchUrl + '?m="Success."', fetchConfig);
-  })
-  .catch((err) => {
-    fetch(config.snitchUrl + '?m="Failed with errors."', fetchConfig);
-  })
+  let config      = require('./config/config.' + (process.env.NODE_ENV || 'local'));
+  let headers     = {'Content-Type': 'application/x-www-form-urlencoded'};
+  let fetchConfig = {method: 'GET', headers};
+
+  util.promiseBatchPerformer(scheduledTasks, (task) => { return evaluate(task) })
+    .then(() => {
+      fetch(config.snitchUrl + '?m="Success."', fetchConfig);
+    })
+    .catch((err) => {
+      fetch(config.snitchUrl + '?m="Failed with errors."', fetchConfig);
+    })
+
+}
+
+module.exports = hourlyInvoke;
+
