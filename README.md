567e48facdbc1b3ba976aa6e6788a73983950136
alexdm0 <alexdemulder@gmail.com>
2021-02-11 10:27:50 +0100

    updated scheduled tasks

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # crownstone-cron
 Running Cronjobs for the Crownstone Cloud
+
+This is running on my personal account as a free dyno, invoked by the Heroku Scheduler.
+It is monitored by a DeadMan's Snitch.
--- src/scheduledTasks.js
+++ src/scheduledTasks.js
@@ -1,10 +1,10 @@
-let cleanActivityRanges  = require('./actions/cleanActivityRanges')
-let cleanActivityLogs    = require('./actions/cleanActivityLogs')
+// let cleanActivityRanges  = require('./actions/cleanActivityRanges')
+// let cleanActivityLogs    = require('./actions/cleanActivityLogs')
 let refreshToonSchedules = require('./actions/refreshToonSchedules')
 
 let scheduledTasks = [
-  {id:"activityRangeMaintenance", action: cleanActivityRanges,  everyNHours: "6" },
-  {id:"activityLogMaintenance",   action: cleanActivityLogs,    everyNHours: "6" },
+  // {id:"activityRangeMaintenance", action: cleanActivityRanges,  everyNHours: "6" },
+  // {id:"activityLogMaintenance",   action: cleanActivityLogs,    everyNHours: "6" },
   {id:"toonSchedule",             action: refreshToonSchedules, everyNHours: "24"},
 ];
 
