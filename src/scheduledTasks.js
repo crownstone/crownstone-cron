cad713f64fd530f17650e7111a0ec0a35f4c70e2
AlexDM0 <alexdemulder@gmail.com>
2022-09-30 13:51:00 +0200

    opened tasks

--- src/scheduledTasks.js
+++ src/scheduledTasks.js
@@ -5,9 +5,9 @@ const aggregateEnergyData   = require("./actions/aggregateEnergyData");
 
 let scheduledTasks = [
   {id:"aggregateEnergyUsage", action: aggregateEnergyData,  everyNHours: "0.5"},
-  // {id:"sanitizeDatabase",     action: sanitizeDatabase,     everyNHours: "12"},
-  // {id:"toonSchedule",         action: refreshToonSchedules, everyNHours: "24"},
-  // {id:"SSE+Webhooks",         action: checkSseAndWebhooks,  everyNHours: "1"},
+  {id:"sanitizeDatabase",     action: sanitizeDatabase,     everyNHours: "12"},
+  {id:"toonSchedule",         action: refreshToonSchedules, everyNHours: "24"},
+  {id:"SSE+Webhooks",         action: checkSseAndWebhooks,  everyNHours: "1"},
 ];
 
 
